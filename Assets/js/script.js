76ab1a3f64d669f4fc82532e51862d162b35667e
JC Berry <jcharlesberry@gmail.com>
2022-07-27 20:54:30 -0700

    never-leaving.html populating and search box for performers working

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,8 +9,6 @@ var sportsCkboxEl = document.getElementById('sportsCkbox');
 var dateDivEl = document.getElementById('dateDiv');
 var resultsEl = document.getElementById('results');
 
-searchBtnEl.addEventListener('click',searchEvents);
-
 var evtCkboxEls = document.querySelectorAll('.evtCkbox');
 
 /*restsCkboxEl.addEventListener('change',includeInSearch);
@@ -27,35 +25,64 @@ sportsCkboxEl.addEventListener('change',includeInSearch);
 function searchEvents(){
 
     // get search phrase
-    var searchPhrase = searchPhraseBoxEl.value;
+    var searchPhrase = searchPhraseBoxEl.value.replace(/ /g, '-');
+
     
     //second, get checked checkboxes
-    for (i=0; i < evtCkboxEls.length; i++){
+    /*for (i=0; i < evtCkboxEls.length; i++){
         if(evtCkboxEls[i].checked){
             //console.log(evtCkboxEls[i].id);
             searchPhrase += '&' + 
             console.log(searchPhrase);
         }
-    }
-    //var requestUrl = "https://api.seatgeek.com/2/events?lat=32.7174202&lon=-117.1627728&client_id=MjgwNTI3ODN8MTY1ODgwNDk2Ny40MzUwODQ4&client_secret=85b20bc2aa3ec76141297b33f744b07b992ea1cfdcd7fabbc802e2057bae02b8";
-    var requestUrl = "https://api.yelp.com/v3/businesses/search?text=&location=coronado&term=restaurant";//&clientID=fCDe8r_Udq8P1Zrq3i3iWA&apiKey=xPeUTskMuMC-scLgkWv4DwxwPfi9f9jiSlKeVQoLIoLYLFF_FLJ8nIhu19ChnP9fSjDnh_tnLQhHpfZpvWKhD6bNmsNLA4Bk_Q-_47BOI7gRcB73AO7g_YR5NLfgYnYx"
+    }*/
+    var requestUrl = `https://api.seatgeek.com/2/events?performers.slug=${searchPhrase}&lat=32.7174202&lon=-117.1627728&client_id=MjgwNTI3ODN8MTY1ODgwNDk2Ny40MzUwODQ4&client_secret=85b20bc2aa3ec76141297b33f744b07b992ea1cfdcd7fabbc802e2057bae02b8`;
+    //console.log(requestUrl);
+    //var requestUrl = URL(string:"https://api.yelp.com/v3/businesses/search?text=&location=coronado&term=restaurant");
+    //&clientID=fCDe8r_Udq8P1Zrq3i3iWA
+    //&apiKey=xPeUTskMuMC-scLgkWv4DwxwPfi9f9jiSlKeVQoLIoLYLFF_FLJ8nIhu19ChnP9fSjDnh_tnLQhHpfZpvWKhD6bNmsNLA4Bk_Q-_47BOI7gRcB73AO7g_YR5NLfgYnYx"
+    // Authorization HTTP header value as Bearer API_KEY
+    //"Authorization: Bearer <YOUR API KEY>"
     //geolocation - https://api.seatgeek.com/2/events?lat=32.7174202&lon=-117.1627728
     // San Diego 32.7174202 -117.1627728
     //console.log(requestUrl);
+    //let apiKey = "your_api_key"
+    //var url = URL(string: "https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972");
+    //var request = URLRequest(url: url);
+    //var requestUrl = "https://api.yelp.com/v3/businesses/search?location=coronado";
+    //var request = URLRequest(requestUrl, requestUrl)
+    //request.addValue("Bearer \xPeUTskMuMC-scLgkWv4DwxwPfi9f9jiSlKeVQoLIoLYLFF_FLJ8nIhu19ChnP9fSjDnh_tnLQhHpfZpvWKhD6bNmsNLA4Bk_Q-_47BOI7gRcB73AO7g_YR5NLfgYnYx", forHTTPHeaderField: "Authorization");
+    //const data = {text: "restaurant"};
+    /*fetch(requestUrl)/*, {
+        method: 'get',
+        headers: {
+            'Authorization': 'Bearer xPeUTskMuMC-scLgkWv4DwxwPfi9f9jiSlKeVQoLIoLYLFF_FLJ8nIhu19ChnP9fSjDnh_tnLQhHpfZpvWKhD6bNmsNLA4Bk_Q-_47BOI7gRcB73AO7g_YR5NLfgYnYx',
+            'Accept': 'application/json, text/plain, /'
+            //'Access-Control-Allow-Origin': 'http://localhost',
+            'Content-Type': 'application/x-www-form-urlencoded'
+            //'Content-Type': 'application/json;charset=utf-8',
+            //'Cache-Control': 'private'
+        },
+        //body: JSON.stringify(data),
+        mode: 'no-cors',
+        cache: 'default',
+    }*/
     fetch(requestUrl)
     .then(function (response) {
-        //console.log('response');
         return response.json();
     })
     .then(function (data) {
        for(var i=0; i < data.events.length; i++){
-        resultsEl.innerHTML += data[i];
-            resultsEl.innerHTML += data.events[i].title + ' will be at ' + data.events[i].venue.name + '<br>';
+        console.log(data.events[i]);
+        document.querySelectorAll('#resultsNames ul li')[i].innerHTML = data.events[i].title;
+        document.querySelectorAll('#resultsAreas ul li')[i].innerHTML = data.events[i].venue.name;
+        document.querySelectorAll('#resultsHours ul li')[i].innerHTML = data.events[i].datetime_utc;
        }
-        console.log(data.events);
     })
   }
 searchEvents();
+
+searchBtnEl.addEventListener('click',searchEvents);
 
 /** YELP\\
 
@@ -90,4 +117,19 @@ virtserver.swaggerhub.com/JCHARLESBERRY_1/sandiegobands/1.0.0
 Google Places API??? {https://developers.google.com/maps/documentation/places/web-service/search}
 Yelp API  --- GET https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972
 
-Seatgeek API (includes sporting events) - Margot */
+Seatgeek API (includes sporting events) - Margot
+
+GET https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972
+GET https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972
+DETAILS GET https://api.yelp.com/v3/businesses/north-india-restaurant-san-francisco
+(REVIEWS GET https://api.yelp.com/v3/businesses/north-india-restaurant-san-francisco/reviews
+)
+Business Search	/businesses/search	Search for businesses by keyword, category, location, price level, etc.
+
+Business Match	/businesses/matches	Find the Yelp business that matches an exact input location. Use this to match business data from other sources with Yelp businesses.
+
+Business Details	/businesses/{id}	Get rich business data, such as name, address, phone number, photos, Yelp rating, price levels and hours of operation.
+
+/events/{id}
+This endpoint returns the detailed information of a Yelp event. You can get the event ID from /events or /events/featured.  GET https://api.yelp.com/v3/events/{id}
+*/
